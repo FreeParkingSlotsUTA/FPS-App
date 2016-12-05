9371e0f6a4cef7f354cbf629b6b1f2356520f0db
Petteri Davidsson <Petteri Davidsson>
2016-12-05 17:23:17 +0200

    Update to slots, not tested

--- Back-end/Parkingslots/slots.php
+++ Back-end/Parkingslots/slots.php
@@ -1,40 +1,49 @@
 <?php
 	
-	/*
+	
 	$dbconn = pg_connect("host=localhost dbname=parking user=null password=null")
 		or die('Could not connect: ' . pg_last_error());
 		
-	$park1 = pg_query('SELECT PAreaId(1), NumberOfSlots, SlotsOccupied FROM ParkingArea....') or die('Query failed: ' . pg_last_error());
-	$park2 = pg_query('SELECT PAreaId(2), NumberOfSlots, SlotsOccupied FROM ParkingArea....') or die('Query failed: ' . pg_last_error());
-	$park3 = pg_query('SELECT PAreaId(3), NumberOfSlots, SlotsOccupied FROM ParkingArea....') or die('Query failed: ' . pg_last_error());
-	*/
+	$result1 = pg_query($dbconn, "SELECT PAreaId(1), NumberOfSlots, SlotsOccupied FROM ParkingArea");
+	if(!$result1){
+		echo "An error occurred. \n";
+		exit;
+	}
+	$result2 = pg_query($dbconn, "SELECT PAreaId(2), NumberOfSlots, SlotsOccupied FROM ParkingArea");
+	if(!$result2){
+		echo "An error occurred. \n";
+		exit;
+	}
+	$result3 = pg_query($dbconn, "SELECT PAreaId(3), NumberOfSlots, SlotsOccupied FROM ParkingArea");
+	if(!$result3){
+		echo "An error occurred. \n";
+		exit;
+	}
+	
+	$row1 = pg_fetch_row($result1);
+	$row2 = pg_fetch_row($result2);
+	$row3 = pg_fetch_row($result3);
+	
 	
 	$park1 = array(
-		'parkId' => 1,
-		'slots' => 5
+		'parkId' => row1[0],
+		'slots' => row1[1] - row1[2]
 	);
 
 	$park2 = array(
-		'parkId' => 2,
-		'slots' => 0
+		'parkId' => row2[0],
+		'slots' => row2[1] - row2[2]
 	);
 
 	$park3 = array(
-		'parkId' => 3,
-		'slots' => 11
+		'parkId' => row3[0],
+		'slots' => row3[1] - row3[2]
 	);
 
 
 	echo json_encode($park1);
 	echo json_encode($park2);
 	echo json_encode($park3);
-	
-	/*
-	pg_free_result($park1);
-	pg_free_result($park2);
-	pg_free_result($park3);
-	pg_close($dbconn);
-	*/
 
 
 ?>
